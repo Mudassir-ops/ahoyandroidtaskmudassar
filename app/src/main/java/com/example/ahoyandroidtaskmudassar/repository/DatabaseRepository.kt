41517902e3b17ad2cfb86a69cb2ac769d4cc08ea
Mudassir-ops <dreame488@gmail.com>
2022-01-26 04:21:16 +0400

    Room Database Hilt Module with Fav List From Db Working

---
+++ app/src/main/java/com/example/ahoyandroidtaskmudassar/repository/DatabaseRepository.kt
@@ -0,0 +1,19 @@
+package com.example.ahoyandroidtaskmudassar.repository
+
+import androidx.annotation.WorkerThread
+import com.example.ahoyandroidtaskmudassar.model.datamodels.clinnic.FavouritesTable
+import com.example.ahoyandroidtaskmudassar.model.datasource.local.dao.FavouriteDao
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class UserRepository @Inject constructor (private val favouriteDao: FavouriteDao) {
+    val getfavourites:Flow<List<FavouritesTable>> = favouriteDao.getAllFavouriteCity()
+
+    @WorkerThread
+    suspend fun insert(favouritesTable: FavouritesTable) = withContext(Dispatchers.IO){
+        favouriteDao.insertFavouriteCity(favouritesTable)
+    }
+}
